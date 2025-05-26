ac777f1bf42d22f18579fdc940d4fa44da7dc9c1
helalscript <husoaib422@gmail.com>
2025-05-26 14:43:36 +0600

    Refactor NewsIndexResource to include language-based translation for title, published date, and author

--- app/Http/Resources/API/V1/NewsIndexResource.php
+++ app/Http/Resources/API/V1/NewsIndexResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\V1;
 
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,11 +15,13 @@ class NewsIndexResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $lang = $request->header('Accept-Language') ?? 'en';
+        // dd($lang);
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'published_at' => $this->published_at->format('d-M-Y'),
-            'author' => $this->author,
+            'title' => $lang === 'en' ? $this->title : Helper::translateCached($this->title, $lang),
+            'published_at' => $lang === 'en' ? $this->published_at->format('d-M-Y') : Helper::translateCached($this->published_at->format('d-M-Y'), $lang),
+            'author' => $lang === 'en' ? $this->author : Helper::translateCached($this->author, $lang),
             'image_url' => $this->image_url,
             'category' => $this->category
         ];
