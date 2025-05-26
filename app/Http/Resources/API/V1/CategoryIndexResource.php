6194311303e45bb2a09e41d86ca1dc6d3f2e3268
helalscript <husoaib422@gmail.com>
2025-05-26 14:43:24 +0600

    Add CategoryIndexResource for category data transformation

---
+++ app/Http/Resources/API/V1/CategoryIndexResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\API\V1;
+
+use App\Helpers\Helper;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $lang = $request->header('Accept-Language') ?? 'en';
+        return [
+            'id' => $this->id,
+            'name' => $lang === 'en' ? $this->name : Helper::translateCached($this->name, $lang),
+            'related_keywords' => $this->related_keywords,
+            'image' => $this->image,
+        ];
+    }
+}
