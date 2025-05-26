81d9d9678d87064297fdc241bae57ed82ad1c53c
helalscript <husoaib422@gmail.com>
2025-05-26 14:43:30 +0600

    Refactor CategoryShowResource to include language-based name translation

--- app/Http/Resources/API/V1/CategoryShowResource.php
+++ app/Http/Resources/API/V1/CategoryShowResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\V1;
 
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,9 +15,10 @@ class CategoryShowResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $lang = $request->header('Accept-Language') ?? 'en';
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'name' => $lang === 'en' ? $this->name : Helper::translateCached($this->name, $lang),
             'related_keywords' => $this->related_keywords,
             'image' => $this->image,
             'news' => NewsShowResource::collection($this->whenLoaded('news')),
