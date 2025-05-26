2439eacc3fd697852751d47f389827451f677e2c
helalscript <husoaib422@gmail.com>
2025-05-26 14:43:41 +0600

    Refactor NewsShowResource to include language-based translation for summary, published date, and author

--- app/Http/Resources/API/V1/NewsShowResource.php
+++ app/Http/Resources/API/V1/NewsShowResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\V1;
 
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,14 +15,15 @@ class NewsShowResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $lang = $request->header('Accept-Language') ?? 'en';
         return [
             'id' => $this->id,
             'category_id' => $this->category_id,
             'url' => $this->url,
-            'summary' => $this->summary,
-            'published_at' => $this->published_at->format('d-M-Y'),
+            'summary' => $lang === 'en' ? $this->summary : Helper::translateCached($this->summary, $lang),
+            'published_at' => $lang === 'en' ? $this->published_at->format('d-M-Y') : Helper::translateCached($this->published_at->format('d-M-Y'), $lang),
             'image_url' => $this->image_url,
-            'author' => $this->author
+            'author' => $lang === 'en' ? $this->author : Helper::translateCached($this->author, $lang),
         ];
     }
 }
